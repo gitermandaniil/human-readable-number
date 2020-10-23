dc1cf7d9b36a0f05c83be721992babe1d6ef15f3
Daniil Giterman <giterman@corpcentre.ru>
2020-10-23 21:42:46 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = num => {
+  if (num === 0) return getWord(num)
+
+  const result = []
+
+  if (num > 99) {
+    result.push(getWord(num / 100))
+    result.push('hundred')
+  }
+
+  if (num % 100 > 19) {
+    result.push(getWord(num % 100 / 10 + 18))
+
+    if (num % 10) {
+      result.push(getWord(num % 10))
+    }
+  } else if (num % 100) {
+    result.push(getWord(num % 100))
+  }
+
+  return result.join(' ')
+}
+
+function getWord (expression) {
+  const dictionary = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  return dictionary[Math.floor(expression)]
 }
